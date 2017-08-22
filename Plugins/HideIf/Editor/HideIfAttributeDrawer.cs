eb01fd09be05378613c0943716af30fcfbae4a9e
baste <baste@rain-games.com>
2017-08-22 21:50:46 +0200

    Initial commit!
    Code should be working and documented. There's even an example file! What more could you want?

---
+++ Plugins/HideIf/Editor/HideIfAttributeDrawer.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public abstract class HidingAttributeDrawer : PropertyDrawer {
+
+    /// <summary>
+    /// Checks if a property is set to be hidden by a HideIfAttribute.
+    /// 
+    /// Usefull for other property drawers that should respect the HideIfAttribute
+    /// </summary>
+    public static bool CheckShouldHide(SerializedProperty property) {
+        try {
+            bool shouldHide = false;
+
+            HidingAttribute[] attachedAttributes =
+                (HidingAttribute[])
+                    property.serializedObject.targetObject.GetType().GetField(property.name).GetCustomAttributes(typeof (HidingAttribute), false);
+
+            foreach (var hider in attachedAttributes) {
+                if (!ShouldDraw(property.serializedObject, hider)) {
+                    shouldHide = true;
+                }
+            }
+
+            return shouldHide;
+        }
+        catch {
+            return false;
+        }
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        if (!CheckShouldHide(property)) {
+            EditorGUI.PropertyField(position, property, true);
+        }
+    }
+
+    public abstract bool ShouldDraw(SerializedObject obj);
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+        //Even if the property height is 0, the property gets margins of 1 both up and down.
+        //So to truly hide it, we have to hack a height of -2 to counteract that! 
+        return !CheckShouldHide(property) ? base.GetPropertyHeight(property, label) : -2f;
+    }
+
+    public static bool ShouldDraw(SerializedObject obj, HidingAttribute hider) {
+        var hideIf = hider as HideIfAttribute;
+        if (hideIf != null) {
+            return HideIfAttributeDrawer.ShouldDraw(obj, hideIf);
+        }
+
+        var hideIfNull = hider as HideIfNullAttribute;
+        if (hideIfNull != null) {
+            return HideIfNullAttributeDrawer.ShouldDraw(obj, hideIfNull);
+        }
+
+        var hideIfNotNull = hider as HideIfNotNullAttribute;
+        if (hideIfNotNull != null) {
+            return HideIfNotNullAttributeDrawer.ShouldDraw(obj, hideIfNotNull);
+        }
+
+        var hideIfEnum = hider as HideIfEnumValueAttribute;
+        if (hideIfEnum != null) {
+            return HideIfEnumValueAttributeDrawer.ShouldDraw(obj, hideIfEnum);
+        }
+
+        Debug.LogWarning("Trying to check unknown hider type: " + hider.GetType().Name);
+        return false;
+    }
+}
+
+[CustomPropertyDrawer(typeof (HideIfAttribute))]
+public class HideIfAttributeDrawer : HidingAttributeDrawer {
+
+    public override bool ShouldDraw(SerializedObject obj) {
+        return ShouldDraw(obj, attribute as HideIfAttribute);
+    }
+
+    public static bool ShouldDraw(SerializedObject obj, HideIfAttribute attribute) {
+        var prop = obj.FindProperty(attribute.variable);
+        if (prop == null) {
+            return true;
+        }
+        return prop.boolValue != attribute.state;
+    }
+}
+
+[CustomPropertyDrawer(typeof (HideIfNullAttribute))]
+public class HideIfNullAttributeDrawer : HidingAttributeDrawer {
+
+    public override bool ShouldDraw(SerializedObject obj) {
+        return ShouldDraw(obj, attribute as HideIfNullAttribute);
+    }
+
+    public static bool ShouldDraw(SerializedObject obj, HideIfNullAttribute hideIfNullAttribute) {
+        var prop = obj.FindProperty(hideIfNullAttribute.variable);
+        if (prop == null) {
+            return true;
+        }
+
+        return prop.objectReferenceValue != null;
+    }
+}
+
+[CustomPropertyDrawer(typeof (HideIfNotNullAttribute))]
+public class HideIfNotNullAttributeDrawer : HidingAttributeDrawer {
+
+    public override bool ShouldDraw(SerializedObject obj) {
+        return ShouldDraw(obj, attribute as HideIfNotNullAttribute);
+    }
+
+    public static bool ShouldDraw(SerializedObject obj, HideIfNotNullAttribute hideIfNotNullAttribute) {
+        var prop = obj.FindProperty(hideIfNotNullAttribute.variable);
+        if (prop == null) {
+            return true;
+        }
+
+        return prop.objectReferenceValue == null;
+    }
+}
+
+[CustomPropertyDrawer(typeof (HideIfEnumValueAttribute))]
+public class HideIfEnumValueAttributeDrawer : HidingAttributeDrawer {
+
+    public override bool ShouldDraw(SerializedObject obj) {
+        return ShouldDraw(obj, attribute as HideIfEnumValueAttribute);
+    }
+
+    public static bool ShouldDraw(SerializedObject obj, HideIfEnumValueAttribute hideIfEnumValueAttribute) {
+        var enumProp = obj.FindProperty(hideIfEnumValueAttribute.variable);
+        var states = hideIfEnumValueAttribute.states;
+        
+        bool equal = false;
+        for(int i = 0; i < states.Length; i++) {
+            if(states[i] == enumProp.intValue) {
+                equal = true; //enumProp.enumValueIndex gives the order in the enum list, not the actual enum value
+                break; 
+            }
+        }
+
+        return equal != hideIfEnumValueAttribute.hideIfEqual;
+    }
+}
---
+++ Plugins/HideIf/HideIfExampleScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideIfExampleScript : MonoBehaviour {
+    
+    public bool showS1;
+    
+    [HideIf("showS1", false)]
+    public string s1 = "S1";
+    [HideIf("showS1", true)]
+    public string s2 = "S2 instead";
+    
+    [Space(10f)]
+    public Object obj;
+    
+    [HideIfNotNull("obj")]
+    public string objIsNull = "Shown as obj is null!";
+    
+    [HideIfNull("obj")]
+    public string objIsNotNull = "Shown as obj is not null!";
+    
+    [Space(10f)]
+    public TestEnum testEnum;
+    
+    [HideIfEnumValue("testEnum", HideIf.Equal, (int) TestEnum.Val1)]
+    public string isNotVal1 = "Val 1 not selected";
+    
+    [HideIfEnumValue("testEnum", HideIf.NotEqual, (int) TestEnum.Val1)]
+    public string isVal1 = "Val 1 selected";
+    
+    [HideIfEnumValue("testEnum", HideIf.Equal, (int) TestEnum.Val2, (int) TestEnum.Val3)]
+    public string isNotVal2Or3 = "Neither Val 2 nor 3 are selected";
+    
+    [HideIfEnumValue("testEnum", HideIf.NotEqual, (int) TestEnum.Val2, (int) TestEnum.Val3)]
+    public string isVal2Or3 = "Val 2 or 3 are selected";
+    
+    public enum TestEnum {
+        Val1,
+        Val2,
+        Val3
+    }
+}
